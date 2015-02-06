ca21ac5afc53431f4807189f380e55eef5ead006
Flo <Flo@172.31.247.84>
2015-02-06 09:28:35 +0100

    createModel

---
+++ crossword/src/crossword/Grid.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crossword;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Flo
+ */
+public class Grid {
+    private int size;
+    private ArrayList<String> listWord;
+    private Square [][] squareGrid;
+    
+}
