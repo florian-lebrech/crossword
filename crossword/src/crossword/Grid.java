9b13e44d8dd22b928f09db039cde88451d509e5f
Flo <Flo@172.31.247.84>
2015-02-06 09:41:57 +0100

    createModel

--- crossword/src/crossword/Grid.java
+++ crossword/src/crossword/Grid.java
@@ -15,5 +15,32 @@ public class Grid {
     private int size;
     private ArrayList<String> listWord;
     private Square [][] squareTable;
+
+    
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public ArrayList<String> getListWord() {
+        return listWord;
+    }
+
+    public void setListWord(ArrayList<String> listWord) {
+        this.listWord = listWord;
+    }
+
+    public Square[][] getSquareTable() {
+        return squareTable;
+    }
+
+    public void setSquareTable(Square[][] squareTable) {
+        this.squareTable = squareTable;
+    }
+    
+    
     
 }
