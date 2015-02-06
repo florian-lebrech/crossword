b47eb9d1677e756ca61797a733520e526973867e
Gamesnerd <Gamesnerd@172.31.247.83>
2015-02-06 10:12:16 +0100

    Square is well done now

--- crossword/src/crossword/Square.java
+++ crossword/src/crossword/Square.java
@@ -11,4 +11,51 @@ package crossword;
  */
 public class Square {
     
+    private char letterActual;
+    private char letterWanted;
+
+    public Square() {
+        this.letterActual = ' ';
+        this.letterWanted = ' ';
+    }
+    
+    public Square(char letterWanted) {
+        this.letterActual = ' ';
+        this.letterWanted = letterWanted;
+    }
+    
+    /**
+     * 
+     * @return if the square is filled with the good letter
+     */
+    public boolean isCorrect(){
+        return letterActual==letterWanted;
+    }
+    
+    /**
+     * 
+     * @return if the square must be black or not
+     */
+    public boolean isBlack(){
+        return letterWanted==' ';
+    }
+
+    public char getLetterActual() {
+        return letterActual;
+    }
+
+    public void setLetterActual(char letterActual) {
+        this.letterActual = letterActual;
+    }
+
+    public char getLetterWanted() {
+        return letterWanted;
+    }
+
+    public void setLetterWanted(char letterWanted) {
+        this.letterWanted = letterWanted;
+    }
+    
+    
+    
 }
