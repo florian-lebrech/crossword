9e4ee298ae285eea24829245f40fc8c5425e5996
Gamesnerd <Gamesnerd@172.31.247.83>
2015-02-09 09:38:32 +0100

    crate the constructor with size and ArrayList of Words

--- crossword/src/crossword/Grid.java
+++ crossword/src/crossword/Grid.java
@@ -15,8 +15,6 @@ public class Grid {
     private int size;
     private ArrayList<Word> listWord;
     private Square [][] squareTable;
-
-     
     
     public int getSize() {
         return size;
@@ -41,10 +39,30 @@ public class Grid {
     public void setSquareTable(Square[][] squareTable) {
         this.squareTable = squareTable;
     }
+    
+    public Grid(ArrayList<Word> listWord, int size){
+        this.listWord=listWord;
+        this.size=size;
+        squareTable=new Square[10][10];
+        
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                squareTable[i][j]= new Square();
+            }
+        }
+        
+        for(Word w:listWord){
+            char c[]=w.getEnglishWord().toCharArray();
+            int i=0;
+            while(c[i]!='\0'){
+                
+                i++;
+            }
+        }
+    }
 
     public Grid() {
+        
     }
-    
-    
     
 }
