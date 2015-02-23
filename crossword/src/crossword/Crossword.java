4200a720a333f4ed5969d5b9c6be6e7fd43bc320
Gamesnerd <Gamesnerd@172.31.247.83>
2015-02-23 10:53:26 +0100

    Grid constructor corrected

--- crossword/src/crossword/Crossword.java
+++ crossword/src/crossword/Crossword.java
@@ -5,6 +5,9 @@
  */
 package crossword;
 
+import View.PrincipalView;
+import java.util.ArrayList;
+
 /**
  *
  * @author Flo
@@ -15,12 +18,21 @@ public class Crossword {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Word w1=new Word("IVRE","DRUNK", 0, 0, true,0);
-       Word w2=new Word("HEUREUX","HAPPY", 4, 0, true,1); 
-       Word w3=new Word("COURIR","RUN", 2, 3, false,2);
-       Word w4=new Word("APPLICATION","APP", 2, 3, false,3);
-       
-       
+        Word w1 = new Word("IVRE", "DRUNK", 0, 0, true, 0);
+        Word w2 = new Word("HEUREUX", "HAPPY", 4, 0, true, 1);
+        Word w3 = new Word("COURIR", "RUN", 2, 3, false, 2);
+        Word w4 = new Word("APPLICATION", "APP", 2, 3, true, 3);
+
+        ArrayList<Word> michel = new ArrayList<>();
+        michel.add(w1);
+        michel.add(w2);
+        michel.add(w3);
+        michel.add(w4);
+
+        Grid jacquie = new Grid(michel, 10);
+        
+        PrincipalView view =new PrincipalView(jacquie);
+
     }
-    
+
 }
