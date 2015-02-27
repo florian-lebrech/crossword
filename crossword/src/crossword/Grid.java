033e85d7d5292c19662abf4f128b727cc3c8081a
Gamesnerd <Gamesnerd@172.31.247.83>
2015-02-27 08:40:41 +0100

    Grid::isThisSquareWordComplete

--- crossword/src/crossword/Grid.java
+++ crossword/src/crossword/Grid.java
@@ -19,16 +19,75 @@ public class Grid {
 
     public boolean isGridFinished() {
         for (int i = 0; i < size; i++) {
-            for(int j=0;j<size;j++){
-                if(!squareTable[i][j].isCorrect()){
+            for (int j = 0; j < size; j++) {
+                if (!squareTable[i][j].isCorrect()) {
                     return false;
                 }
             }
         }
-        
+
         return true;
     }
-    
+
+    public boolean isThisSquareWordComplete(int x, int y) {
+        if (x < 0 || x > size - 1 || y < 0 || y > size - 1) {
+            return false;
+        }
+
+        boolean wordDone = true;
+
+        //first test on top of the square
+        int i = x, j = y;
+        if ((i >= 1 && !squareTable[i - 1][j].isBlack()) && (i <= size - 2 && !squareTable[i + 1][j].isBlack())) {
+            while ((i > 0 && i < size - 1 && j > 0 && j < size - 1 && !squareTable[i][j].isBlack())) {
+                if (!squareTable[i][j].isCorrect()) {//if a letter is not the wanted one, the word isn't yet accepted
+                    wordDone = false;
+                }
+                i--;
+            }
+
+            if (wordDone) {//if the top part of the word is good we try the bottom one
+                i = x;
+                j = y;
+                while ((i > 0 && i < size - 1 && j > 0 && j < size - 1 && !squareTable[i][j].isBlack())) {
+                    if (!squareTable[i][j].isCorrect()) {//if a letter is not the wanted one, the word isn't yet accepted
+                        wordDone = false;
+                    }
+                    i++;
+                }
+            }
+        }
+
+        if (wordDone) {//if the wordDone is still true, the vertical word is good
+            return true;
+        }
+
+        //else, let's try the horizontal word
+        wordDone = true;
+        i = x;
+        j = y;
+        while ((i > 0 && i < size - 1 && j > 0 && j < size - 1 && !squareTable[i][j].isBlack())) {
+            if (!squareTable[i][j].isCorrect()) {//if a letter is not the wanted one, the word isn't yet accepted
+                wordDone = false;
+            }
+            j--;
+        }
+
+        if (wordDone) {//if the left part of the word is good we try the right one
+            i = x;
+            j = y;
+            while ((i > 0 && i < size - 1 && j > 0 && j < size - 1 && !squareTable[i][j].isBlack())) {
+                if (!squareTable[i][j].isCorrect()) {//if a letter is not the wanted one, the word isn't yet accepted
+                    wordDone = false;
+                }
+                j++;
+            }
+        }
+
+        return wordDone;
+
+    }
+
     public int getSize() {
         return size;
     }
