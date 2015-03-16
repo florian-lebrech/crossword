dcde92acf6ac78cde41140bf9a275b2a2105dfe0
Gamesnerd <Gamesnerd@172.31.247.83>
2015-03-16 11:38:15 +0100

    Added export function

--- crossword/src/View/PrincipalView.java
+++ crossword/src/View/PrincipalView.java
@@ -5,13 +5,17 @@
  */
 package View;
 
-
 import crossword.Grid;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -32,7 +36,7 @@ public class PrincipalView extends JFrame implements ActionListener {
     private GridView gridView;
     private DefinitionView defView;
     private GridBagConstraints cont;
-    
+
     public PrincipalView(Grid g) {
         grid = g;
         menu = new JMenu("menu");
@@ -42,75 +46,80 @@ public class PrincipalView extends JFrame implements ActionListener {
         barmenu = new JMenuBar();
         try_again.addActionListener(this);
         export.addActionListener(this);
-       
-        cont=new GridBagConstraints();
-        cont.fill=GridBagConstraints.BOTH;
+
+        cont = new GridBagConstraints();
+        cont.fill = GridBagConstraints.BOTH;
         principalpanel.setLayout(new GridBagLayout());
         init();
         this.repaint();
         this.pack();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
-        
+
     }
 
     public void init() {
-         gridView=new GridView(grid);
-        defView=new DefinitionView(grid);
+        gridView = new GridView(grid);
+        defView = new DefinitionView(grid);
         menu.add(try_again);
         menu.add(export);
         barmenu.add(menu);
-        cont.gridx=1;
-        cont.gridy=1;
-        cont.gridwidth=1;
+        cont.gridx = 1;
+        cont.gridy = 1;
+        cont.gridwidth = 1;
         this.addMouseListener(gridView);
         this.addKeyListener(gridView);
-        gridView.paintImmediately(0, 0, gridView.getWidth(),gridView.getHeight());
-        principalpanel.add(gridView,cont);
-        
-        cont.gridwidth=1;
-        cont.gridx=2;
-        principalpanel.add(defView,cont);
-        
+        gridView.paintImmediately(0, 0, gridView.getWidth(), gridView.getHeight());
+        principalpanel.add(gridView, cont);
+
+        cont.gridwidth = 1;
+        cont.gridx = 2;
+        principalpanel.add(defView, cont);
+
        // principalpanel.repaint();
-        
         principalpanel.validate();
         this.setContentPane(principalpanel);
         this.setJMenuBar(barmenu);
-       
+
         //this.repaint();
-        this.setPreferredSize(new Dimension(1200,700));
+        this.setPreferredSize(new Dimension(1200, 700));
         //this.pack();
         //this.validate();
-        
+
         this.setName("CrossWord :");
         this.setTitle("CrossWord");
-        
-        
-        
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        if(e.getSource()==try_again){
-            for(int i=0;i<grid.getSize();i++){
-                for(int j=0;j<grid.getSize();j++){
+        if (e.getSource() == try_again) {
+            for (int i = 0; i < grid.getSize(); i++) {
+                for (int j = 0; j < grid.getSize(); j++) {
                     grid.getSquareTable()[i][j].setLetterActual(' ');
                 }
             }
             this.setVisible(false);
             this.getContentPane().removeAll();
-            grid=crossword.Crossword.try_generate();
+            grid = crossword.Crossword.try_generate();
             init();
-            
+
             this.repaint();
             this.pack();
             this.setVisible(true);
         }
-        if(e.getSource()==export){
-            
-            
+        if (e.getSource() == export) {
+            BufferedImage bi = new BufferedImage(this.getSize().width, this.getSize().height, BufferedImage.TYPE_INT_ARGB);
+            Graphics g = bi.createGraphics();
+            this.paint(g);  //this == JComponent
+            g.dispose();
+            try {
+                ImageIO.write(bi, "png", new File("test.png"));
+            } catch (IOException ex) {
+
+            }
+
         }
     }
 }
